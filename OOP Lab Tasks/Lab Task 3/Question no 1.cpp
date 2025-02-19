39519114c31db31aaafd8544e5c14f1934930e7b
ghulammurtaza-id <112563331+ghulammurtaza-id@users.noreply.github.com>
2025-02-19 13:00:55 +0500

    Update Question no 1.cpp

--- OOP Lab Tasks/Lab Task 3/Question no 1.cpp
+++ OOP Lab Tasks/Lab Task 3/Question no 1.cpp
@@ -1,7 +1,29 @@
 #include<bits/stdc++.h>
+
+
 using namespace std;
-int main(){
+class user {
+ 	public:
+	int age ;
+ 	string Name;
+	private :
+	string getName(){
+		return Name;	
+	}
+	int getage(){
+		return age;
+	}
 	
+};
+
+int main(int argc,char ** argv){
+
+	user monis;
+	monis.age = stoi(argv[2]);
+	monis.Name = argv[1];
+	cout<< "My name is "<< monis.Name<<" ." <<endl;
+	cout<< "I am "<< monis.age<< "year Old"<< endl;
 	
 	return 0;
 }
+	
